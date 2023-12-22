6b6cf7abb3befb2908bd0e604f73fb3396d311f2
swirlz <111631956+swirlz-dev@users.noreply.github.com>
2023-12-23 02:02:45 +0300

    Add files via upload

---
+++ TinyBin/TinyBin/Source/Data/Bin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TinyBin.Source.WinAPI;
+
+namespace TinyBin.Source.Data
+{
+    internal class Bin
+    {
+        public static void OpenFolder() => System.Diagnostics.Process.Start("explorer.exe",
+            "shell:RecycleBinFolder");
+        public static void Empty()
+        {
+            if (GetCount() == 0) MessageBox.Show("Recycle Bin is empty.");
+            else shell32.SHEmptyRecycleBin(IntPtr.Zero, null,
+                shell32.RecycleFlags.SHRB_NOCONFIRMATION | shell32.RecycleFlags.SHRB_NOPROGRESSUI);
+        }
+        public static int GetCount()
+        {
+            shell32.SHQUERYRBINFO sqrbi = new shell32.SHQUERYRBINFO();
+            sqrbi.cbSize = Marshal.SizeOf(typeof(shell32.SHQUERYRBINFO));
+            int hresult = shell32.SHQueryRecycleBin(string.Empty, ref sqrbi);
+            return (int)sqrbi.i64NumItems;
+        }
+    }
+}
---
+++ TinyBin/TinyBin/Source/Data/BinaryDataWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyBin.Source.Data
+{
+    internal class BinaryDataWriter
+    {
+        public static void WriteToFile(int updateTime, bool showEmpty, bool ShowOpen)
+        {
+            try
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(Constants.DATA_FILE_NAME, FileMode.Create)))
+                {
+                    writer.Write(updateTime);
+                    writer.Write(showEmpty);
+                    writer.Write(ShowOpen);
+                }
+            }
+            catch (Exception ex){}
+        }
+    }
+}
